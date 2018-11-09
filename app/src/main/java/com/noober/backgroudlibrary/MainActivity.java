8fb348eb07d61cdce14b0ae2bba0003922cc8e01
macbookair <717219917@qq.com>
2018-11-09 19:09:26 +0800

    测试链接github

--- app/src/main/java/com/noober/backgroudlibrary/MainActivity.java
+++ app/src/main/java/com/noober/backgroudlibrary/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         BackgroundLibrary.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //22 test
     }
 }
